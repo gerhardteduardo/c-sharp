a4abaf18f59d75b4dff5c27dfc56cefce4c4e4cc
Eduardo Gerhardt <comercial.eduardogerhardt@gmail.com>
2026-01-26 23:20:27 -0300

    feat: add generic type

--- reflection/Class1.cs
+++ reflection/Class1.cs
@@ -2,5 +2,16 @@
 
 public class Class1
 {
+    private readonly int test;
 
+    public Class1() 
+    {
+        test = Generic<int>();
+    }
+
+    private static T Generic<T>()
+    {
+        T teste = Activator.CreateInstance<T>();
+        return teste;
+    }
 }
